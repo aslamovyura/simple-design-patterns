8918b873231df6dcaef261d859c4b9390c47d091
Aslamov Yury <aslamov.yura@gmail.com>
2021-12-24 20:15:16 +0300

    Added FactoryMethod

---
+++ Creational/FactoryMethod/ConcreteCreator1.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.Creational.FactoryMethod
+{
+    public class ConcreteCreator1 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct1();
+        }
+    }
+}
---
+++ Creational/FactoryMethod/ConcreteProduct2.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.Creational.FactoryMethod
+{
+    public class ConcreteProduct2 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of ConcreteProduct2}";
+        }
+    }
+}
