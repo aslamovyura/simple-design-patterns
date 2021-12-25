43599aa05d3d01669cc186d8cd2631b11e6f4898
Aslamov Yury <aslamov.yura@gmail.com>
2021-12-25 13:44:54 +0300

    Added Builder.

---
+++ Creational/Builder/ConcreteBuilder.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.Creational.Builder
+{
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product _product = new();
+
+        public ConcreteBuilder() => Reset();
+
+        public void Reset()
+        {
+            _product = new Product();
+        }
+
+        // All production steps work with the same product instance.
+        public void BuildPartA() => _product.Add("PartA1");
+        public void BuildPartB() => _product.Add("PartB1");
+        public void BuildPartC() => _product.Add("PartC1");
+
+        public Product GetProduct()
+        {
+            Product result = _product;
+            Reset();
+
+            return result;
+        }
+    }
+}
---
+++ Creational/Builder/Director.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.Creational.Builder
+{
+    public class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+
+        public void BuildMinimalViableProduct() => _builder.BuildPartA();
+
+        public void BuildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+    }
+}
